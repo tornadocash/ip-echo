ba0d8d5c6d82f77c39f90ec592bf6537fcc4c4aa
poma <Semenov.Roman@mail.ru>
2019-10-06 09:18:49 +0300

    initial

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:11
+
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+CMD ["node", "main.js"]
---
+++ main.js
@@ -0,0 +1,16 @@
+const geoip = require('geoip-lite')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	const ip = req.headers['x-real-ip'] || req.connection.remoteAddress
+	const geo = geoip.lookup(ip) || {}
+	geo.ip = ip
+	res.send(geo)
+})
+
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
